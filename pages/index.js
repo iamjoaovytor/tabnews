43fe27eeda947e2c7909ead82e7917c48aaa6f6d
João Vitor Sousa <57421236+iamjoaovytor@users.noreply.github.com>
2024-12-10 15:36:56 +0000

    change construction phrase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-  return <h1>te amo mikinha s2</h1>;
+  return (
+    <div>
+      <h1> Em Construção </h1>
+      <h2>
+        Auxiliar universitários a organizar seu curso, para alunos que se sentem
+        meio perdido nesse percurso importante.
+      </h2>
+    </div>
+  );
 }
 
 export default Home;
