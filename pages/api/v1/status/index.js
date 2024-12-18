764bdd2789e3751d37029569109b15605ee4b4e2
João Vitor Sousa <57421236+iamjoaovytor@users.noreply.github.com>
2024-12-18 19:23:44 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-  response.status(200).json({ chave: "joão ama a mikinhaaa" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1+1 as sum;");
+  console.log(result.rows);
+  response.status(200).json({ status: "rota /status está em desenvolvimento" });
 }
 
 export default status;
